8f010ed2d91d98dd56a0406dc01aeb803ee946bc
vkr-007 <vikasrajput120406@gmail.com>
2026-08-02 18:35:56 +0530

    Time: 5 ms (26.02%) | Memory: 9.7 MB (76.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,16 +3,14 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
         unordered_map<char,int> v1;
-
         for(auto ch:s){
            v1[ch]++;
         }
-         unordered_map<char,int> v2;
-
         for(auto ch:t){
-           v2[ch]++;
+           v1[ch]--;
+           if (v1[ch] < 0)
+                return false;
         }
-        return v1==v2;
-
+           return true;
     }
 };
