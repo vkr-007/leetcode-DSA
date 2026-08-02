74e07880e97867e401320b76211a73a266158d43
vkr-007 <vikasrajput120406@gmail.com>
2026-08-02 21:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (93.00%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        unordered_map<char,int> v1;
-        for(auto ch:s){
-           v1[ch]++;
-        }
-        for(auto ch:t){
-           v1[ch]--;
-           if (v1[ch] < 0)
-                return false;
-        }
-           return true;
+    if(s.size()!=t.size())return false;
+     int freq[26]={0};
+     for(auto c:s){
+        freq[c-'a']++;
+     }
+     for(auto c:t){
+        freq[c-'a']--;
+        if(freq[c-'a']<0)return false;
+     }
+     return true;
     }
 };
