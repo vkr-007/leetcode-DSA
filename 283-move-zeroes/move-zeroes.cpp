9134f850d07f31865f3ffaa65f4dacf2b3141610
vkr-007 <vikasrajput120406@gmail.com>
2026-08-12 14:21:55 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (56.69%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> temp;
         int k=0;
-        for(auto x: nums){
-            if(x!=0){
-                temp.push_back(x);
-            }else{
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[k]=nums[i];
                 k++;
-            }}
-            while(k!=0){
-                temp.push_back(0);
-                k--;
             }
-        nums= temp;
+        }
+            while(k<nums.size()){
+                nums[k]=0;
+                k++;
+            }
 
     }
 };
