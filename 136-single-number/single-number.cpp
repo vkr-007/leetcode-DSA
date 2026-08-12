0ad41ad216e6dd9c8259fcbf3b649893abb3c607
vkr-007 <vikasrajput120406@gmail.com>
2026-08-12 17:46:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (59.38%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-          unordered_map<int,int> freq;
-        for(auto x: nums){
-            freq[x]++;
-        }
-        for(auto x:freq){
-            if(x.second==1){
-                return x.first;
-            }
-        }
-        return -1;
-    }
+          int ans=0;
+          for(int i=0;i<nums.size();i++){
+            ans= ans^nums[i];
+          }
+          return ans;}
 };
