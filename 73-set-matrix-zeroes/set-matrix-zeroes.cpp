b5a68f20af840eb77897013bd4d1989f612fe84c
vkr-007 <vikasrajput120406@gmail.com>
2026-08-13 16:48:45 +0530

    Time: 3 ms (26.85%) | Memory: 21.2 MB (17.37%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  
+    void setZeroes(vector<vector<int>>& v) {
+        set<int> r;
+        set<int> c;
+        for(int i=0;i<v.size();i++){
+            for(int j=0;j<v[0].size();j++){
+                if(v[i][j]==0){
+                   r.insert(i);
+                   c.insert(j);
+                }
+            }
+        }
+        for(auto x: r){
+            for(int j=0;j<v[0].size();j++){
+                v[x][j]=0;
+            }
+        }
+         for(auto x: c){
+            for(int i=0;i<v.size();i++){
+                v[i][x]=0;
+            }
+        }
+
+    }
+};
