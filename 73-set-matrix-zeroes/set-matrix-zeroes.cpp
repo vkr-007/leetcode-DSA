c43e5bddf4ac7216fded2e2a78d0b38889dc6bd2
vkr-007 <vikasrajput120406@gmail.com>
2026-08-13 16:56:54 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (36.44%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
   
     void setZeroes(vector<vector<int>>& v) {
-        unordered_set<int> r;
-        unordered_set<int> c;
+        vector<bool> r (v.size(),false);
+        vector<bool> c(v[0].size(),false);
         for(int i=0;i<v.size();i++){
             for(int j=0;j<v[0].size();j++){
                 if(v[i][j]==0){
-                   r.insert(i);
-                   c.insert(j);
+                   r[i]= true;
+                   c[j]= true;
                 }
             }
         }
-        for(auto x: r){
-            for(int j=0;j<v[0].size();j++){
-                v[x][j]=0;
-            }
-        }
-         for(auto x: c){
-            for(int i=0;i<v.size();i++){
-                v[i][x]=0;
-            }
-        }
+         for(int i = 0; i < v.size(); i++) {
+        for(int j = 0; j < v[0].size(); j++) {
+            if(r[i] || c[j]) {
+                v[i][j] = 0;        }}}
 
     }
 };
