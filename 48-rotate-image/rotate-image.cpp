7cd5929d97681dde145cfafe62c14d570d670987
vkr-007 <vikasrajput120406@gmail.com>
2026-08-14 18:11:17 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (37.87%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& v) {
         int r=v.size();
-        int c= v[0].size();
-        vector<vector<int>> t(c, vector<int>(r));
         for(int i =0;i<r;i++){
-            for(int j=0;j<c;j++){
-                t[j][i]=v[i][j];
+            for(int j=i+1;j<r;j++){
+               swap(v[i][j], v[j][i]);
             }
         }
        for(int i=0;i<r;i++){
-        reverse(t[i].begin(),t[i].end());
+        reverse(v[i].begin(),v[i].end());
        }
-       v=t;
 
     }
 };
