90e2be16a5e028da7b706cfbe3a1ca4e17eb9088
vkr-007 <vikasrajput120406@gmail.com>
2026-08-15 02:33:25 +0530

    Time: 3 ms (44.05%) | Memory: 8.7 MB (36.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-
-        int i = 0;
-        string str = to_string(x);
-        int j = str.size() - 1;
-
-        while(i < j) {
-            if(str[i] != str[j]) {
-                return false;
-            }
-
-            i++;
-            j--;
+        
+        if (x < 0) {
+            return false;
         }
 
-        return true;
+        long long n = x;
+        long long original = n;
+        long long r = 0;
+
+        while (n > 0) {
+            r = r * 10 + n % 10;
+            n = n / 10;
+        }
+
+        if (r == original) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 };
