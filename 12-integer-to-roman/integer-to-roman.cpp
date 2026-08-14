e77098d1c48a422255240578a1b0162d513b35d4
vkr-007 <vikasrajput120406@gmail.com>
2026-08-14 12:28:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (89.44%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string intToRoman(int num) {
-                int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         string symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         string result = "";
         
