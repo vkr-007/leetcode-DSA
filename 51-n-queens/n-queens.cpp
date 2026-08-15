d34442a811485d6c9ae16dc60dee810417fc37d1
vkr-007 <vikasrajput120406@gmail.com>
2026-08-15 15:19:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (33.38%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -39,7 +39,8 @@ public:
                     solve(b, r + 1, n);
 
                     // backtrack, removing queen
-                    b[r][c] = '.';
+                    b[r][c] = '.';// helps to chec every posibility
+                    
                 }
             }
         }
