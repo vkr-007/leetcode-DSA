0600619b108499e1b3ff5623bacd8df14271d06b
vkr-007 <vikasrajput120406@gmail.com>
2026-08-15 23:07:16 +0530

    Time: 0 ms (100.00%) | Memory: 171.1 MB (100.00%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.cpp
@@ -4,14 +4,14 @@ public:
         int x=0;
         bool neg = false;
         for(auto e:nums){
-            x^=e;
+            x^=e;// checking xor of all
             if(e!=0){
-               neg= true;
+               neg= true;// checking for non zero element
             }
         }
-        if(x!=0) return nums.size();
+        if(x!=0) return nums.size();// xor of all is non zero
         if(neg){
-            return nums.size()-1;
+            return nums.size()-1;// zero
         }
         return 0;
     }
