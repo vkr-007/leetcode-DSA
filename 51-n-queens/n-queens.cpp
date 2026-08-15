d3b1502aa9dc880e4dc0f8b30711c527b0bfff66
vkr-007 <vikasrajput120406@gmail.com>
2026-08-15 15:16:11 +0530

    Time: 3 ms (61.41%) | Memory: 10.5 MB (33.38%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    bool issafe(vector<string>& b, int r, int c, int n) {
+        // column
+        for (int i = 0; i < r; i++) {
+            if (b[i][c] == 'Q') {
+                return false;
+            }
+        }
+        // -ve dig
+        for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
+            if (b[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // +ve dig
+        for (int i = r - 1, j = c + 1; i >= 0 && j < n; i--, j++) {
+            if (b[i][j] == 'Q') {
+                return false;
+            }
+           
+        } return true;
+        }
+
+        void solve(vector<string> & b, int r, int n) {
+            // all queens are placed
+            if (r == n) {
+                ans.push_back(b);
+                return;
+            }
+            // tryin every col
+            for (int c = 0; c < n; c++) {
+                if (issafe(b, r, c, n)) {
+                    // place the queen
+                    b[r][c] = 'Q';
+
+                    // movig to next row
+                    solve(b, r + 1, n);
+
+                    // backtrack, removing queen
+                    b[r][c] = '.';
+                }
+            }
+        }
+
+        vector<vector<string>> solveNQueens(int n) {
+            vector<string> b(n, string(n, '.'));
+            solve(b, 0, n);
+            return ans;
+        }
+    };
