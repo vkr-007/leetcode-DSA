a1be04a0f3c09a82a27029603b8b047fdac86892
vkr-007 <vikasrajput120406@gmail.com>
2026-08-15 15:34:36 +0530

    Time: 3 ms (40.14%) | Memory: 9 MB (23.05%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    bool issafe(vector<string> &b, int r, int c, int n){
+        // col
+        for(int i=0;i<n;i++){
+            if(b[i][c]=='Q') return false;
+
+        }
+        // -ve dig
+        for(int i= r-1, j= c-1;i>=0 && j>=0;i--,j--){
+            if(b[i][j]=='Q') return false;
+        }
+          for(int i= r-1, j= c+1;i>=0 && j<n;i--,j++){
+            if(b[i][j]=='Q') return false;
+        }
+        return true;
+    }
+    void solve(vector<string> &b, int r, int n){
+        if(r==n){
+            ans.push_back(b);
+            return ;
+        }
+        for(int c=0;c<n;c++){
+            if(issafe(b,r,c,n)){
+                b[r][c]='Q';
+                solve(b,r+1,n);
+                b[r][c]='.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<string> b(n,string(n,'.'));
+        solve(b,0,n);
+        return ans.size();
+    }
+};
