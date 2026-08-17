15b893e540886638b5ff3ef3be330bbb2707585b
vkr-007 <vikasrajput120406@gmail.com>
2026-08-17 09:06:38 +0530

    Time: 16 ms (70.62%) | Memory: 28.4 MB (65.65%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char> st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]!='*'){
-                st.push(s[i]);
-            }else{
-                st.pop();
-            }
+        string ans;
 
+        for(char c : s) {
+            if(c == '*') {
+                ans.pop_back();
+            } else {
+                ans.push_back(c);
+            }
         }
-        string ans="";
-       while(!st.empty()){
-        ans+=st.top();
-        st.pop();
-       }
-        reverse(ans.begin(),ans.end());
+
         return ans;
     }
 };
