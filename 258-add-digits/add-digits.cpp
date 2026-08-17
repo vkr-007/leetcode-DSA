93c2540940a1aba9700ff715f43d91a8b9c5efc3
vkr-007 <vikasrajput120406@gmail.com>
2026-08-17 14:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (80.05%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,19 +1,8 @@
 class Solution {
 public:
-    int helper(int num){
-           int ans=0;
-
-        while(num>0){
-            ans += num%10;
-            num/=10;
-        }
-        return ans;
-    }
     int addDigits(int num) {
-       while(num>9){
-        num= helper(num);
-       }
-       return num;
+       if(num==0) return 0;
+       return 1+ (num-1)%9;
 
     }
 };
