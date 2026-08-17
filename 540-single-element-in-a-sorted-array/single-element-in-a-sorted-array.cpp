12b565f1bbbcf2b6d0e26da0ed2e5c6a9b94697e
vkr-007 <vikasrajput120406@gmail.com>
2026-08-17 13:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (36.36%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,31 +4,19 @@ public:
         int lo = 0;
         int hi = nums.size() - 1;
 
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2; 
+            if(mid%2==1){
+                mid--;
+            }
+            if(nums[mid]==nums[mid+1]){
+                lo= mid+2;
+            }else{
+                hi= mid;
+            }
+            
 
-            // single element found
-            if ((mid == 0 || nums[mid] != nums[mid - 1]) &&
-                (mid == nums.size() - 1 || nums[mid] != nums[mid + 1])) {
-                return nums[mid];
-            }
-
-            if (mid > 0 && nums[mid] == nums[mid - 1]) {
-                if ((mid - 1) % 2 == 0) {
-                    lo = mid + 1;
-                } else {
-                    hi = mid - 2;
-                }
-            }
-            else {
-                if (mid % 2 == 0) {
-                    lo = mid + 2;
-                } else {
-                    hi = mid - 1;
-                }
-            }
         }
-
-        return -1;
+        return nums[lo];
     }
 };
