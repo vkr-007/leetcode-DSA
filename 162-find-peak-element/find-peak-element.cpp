9290456f994bd20bed77fdf8cd70117f87924444
vkr-007 <vikasrajput120406@gmail.com>
2026-08-18 11:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (8.07%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int lo= 0;
+        int hi= nums.size()-1;
+        while(lo<hi){
+            int mid= lo+(hi-lo)/2;
+            if(nums[mid]>nums[mid+1]){
+                hi= mid;
+            }else{
+                lo= mid+1;
+            }
+        }
+        return lo;
+    }
+};
