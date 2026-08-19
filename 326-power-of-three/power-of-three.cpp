07cb6a07478459cf406086e6c586fa895f00514e
vkr-007 <vikasrajput120406@gmail.com>
2026-08-19 10:50:03 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (43.89%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0) return false;
-        while(n%3==0){
-            n=n/3;
+          if (n <= 0){ 
+        return false;
         }
-        return n==1;
+        while (n % 3 == 0) {
+            n =n / 3;
+        }
+        if (n == 1) {
+            return true;
+        }else{
+        return false;
+        }  
     }
 };
-
-//pp
