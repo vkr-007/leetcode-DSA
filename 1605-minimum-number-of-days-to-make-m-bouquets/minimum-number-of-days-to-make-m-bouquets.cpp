3477d394f5a0b9ec205b82ba7cc197dd199bf127
vkr-007 <vikasrajput120406@gmail.com>
2026-08-21 17:17:34 +0530

    Time: 20 ms (68.78%) | Memory: 70.4 MB (80.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,36 +1,42 @@
 class Solution {
 public:
     int minDays(vector<int>& v, int m, int k) {
-        if ((long long)m * k > v.size())
+
+        if((long long)m * k > v.size())
             return -1;
+
         int lo = *min_element(v.begin(), v.end());
         int hi = *max_element(v.begin(), v.end());
-        while (lo <= hi) {
+
+        while(lo <= hi) {
+
             int mid = lo + (hi - lo) / 2;
-            int flower = 0;
-            int bouquets = 0;
 
-            for (auto x : v) {
+            int cnt = 0;
+            int b = 0;
 
-                if (x <= mid) {
-                    flower++;
+            for(auto x : v) {
 
-                    if (flower == k) {
-                        bouquets++;
-                        flower = 0;
-                        if (bouquets == m)
+                if(x <= mid) {
+                    cnt++;
+
+                    if(cnt == k) {
+                        b++;
+                        cnt = 0;
+
+                        if(b == m)
                             break;
                     }
-                } else {
-                    flower = 0;
+                }
+                else {
+                    cnt = 0;
                 }
             }
 
-            if (bouquets >= m) {
+            if(b >= m)
                 hi = mid - 1;
-            } else {
+            else
                 lo = mid + 1;
-            }
         }
 
         return lo;
