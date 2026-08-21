fc4c0cb9db060fddeb5135925556cf479e7e57ff
vkr-007 <vikasrajput120406@gmail.com>
2026-08-21 17:09:26 +0530

    Time: 19 ms (80.22%) | Memory: 70.4 MB (80.78%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool possible(vector<int>& v, int days, int m, int k) {
+        int flower = 0;
+        int bouqets = 0;
+        for (auto x : v) {
+            if (x <= days) {
+                flower++;
+                if (flower == k) {
+                    bouqets++;
+                    flower = 0;
+                }
+            } else {
+                flower = 0;
+            }
+        }
+        return bouqets >= m;
+    }
+    int minDays(vector<int>& v, int m, int k) {
+        if ((long long)m * k > v.size())
+            return -1;
+        int lo = *min_element(v.begin(), v.end());
+        int hi = *max_element(v.begin(), v.end());
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (possible(v, mid, m, k)) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+};
