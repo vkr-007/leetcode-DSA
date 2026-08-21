bc227a39a7553af68d56d55287b3c3a47188eeb0
vkr-007 <vikasrajput120406@gmail.com>
2026-08-21 17:14:19 +0530

    Time: 24 ms (41.53%) | Memory: 70.5 MB (80.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    bool possible(vector<int>& v, int days, int m, int k) {
-        int flower = 0;
-        int bouqets = 0;
-        for (auto x : v) {
-            if (x <= days) {
-                flower++;
-                if (flower == k) {
-                    bouqets++;
-                    flower = 0;
-                }
-            } else {
-                flower = 0;
-            }
-        }
-        return bouqets >= m;
-    }
-    //possible checks if no of days is sufficentt or not
     int minDays(vector<int>& v, int m, int k) {
         if ((long long)m * k > v.size())
             return -1;
@@ -25,7 +8,25 @@ public:
 
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if (possible(v, mid, m, k)) {
+            int flower = 0;
+            int bouquets = 0;
+
+            for (auto x : v) {
+
+                if (x <= mid) {
+                    flower++;
+
+                    if (flower == k) {
+                        bouquets++;
+                        flower = 0;
+                    }
+                } else {
+                    flower = 0;
+                }
+            }
+
+
+            if (bouquets >= m) {
                 hi = mid - 1;
             } else {
                 lo = mid + 1;
