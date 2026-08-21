c83792aece1c37ccd4c2b6899bbc3587fe414e50
vkr-007 <vikasrajput120406@gmail.com>
2026-08-21 17:15:25 +0530

    Time: 11 ms (99.29%) | Memory: 70.6 MB (48.84%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -19,6 +19,8 @@ public:
                     if (flower == k) {
                         bouquets++;
                         flower = 0;
+                         if(bouquets == m)
+                break;
                     }
                 } else {
                     flower = 0;
