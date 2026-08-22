653e9f1e9f1d068f74724b9cad6f865b8c80988e
vkr-007 <vikasrajput120406@gmail.com>
2026-08-22 13:37:07 +0530

    Time: 3 ms (66.13%) | Memory: 9.9 MB (89.58%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size()>s2.size()) return false;
+        vector<int> v1(26, 0);
+        vector<int> v2(26, 0);
+        for (auto x : s1) {
+            v1[x - 'a']++;
+        }
+        int k = s1.size();
+        for (int i = 0; i < k; i++) {
+            v2[s2[i] - 'a']++;
+        }
+
+        if (v1 == v2)
+            return true;
+
+        for (int i = k; i < s2.size(); i++) {
+            v2[s2[i] - 'a']++;
+            v2[s2[i - k]-'a']--;
+            if (v1 == v2)
+                return true;
+        }
+        return false;
+    }
+};
