65e39fd32cc2d5ead7e05cb45c49f376b31ac11b
vkr-007 <vikasrajput120406@gmail.com>
2026-08-22 11:52:03 +0530

    Time: 24 ms (5.41%) | Memory: 13.2 MB (59.40%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int v=0;//sliding winow to count vowels
-        for(int i=0;i<k;i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
-              v++;
-            }
+        int count = 0;
+
+        for(int i = 0; i < k; i++){
+            if(string("aeiou").find(s[i]) != string::npos)
+                count++;
         }
-        int vi=v;
-        for(int i=k;i<s.size();i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
-              vi++;
-            }
-            if(s[i-k]=='a'||s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u'){
-              vi--;
-            }
-            v= max(vi,v);
+
+        int ans = count;
+
+        for(int i = k; i < s.size(); i++){
+            if(string("aeiou").find(s[i]) != string::npos)
+                count++;
+
+            if(string("aeiou").find(s[i-k]) != string::npos)
+                count--;
+
+            ans = max(ans, count);
         }
-        return v;
+
+        return ans;
     }
 };
