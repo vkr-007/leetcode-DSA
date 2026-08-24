0b063e4e32da9147137220ec117e1a96be8db285
vkr-007 <vikasrajput120406@gmail.com>
2026-08-25 00:04:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.36%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    string reversed(string &s){
-          reverse(s.begin(),s.end());
-          reverse(s.begin(),s.begin()+1);
-          reverse(s.begin()+1,s.end());
-          return s;
-    }
     bool rotateString(string s, string goal) {
-        int n= s.size();
-        while(n>0){
-            reversed(s);
-            if(s==goal){
-                return true;
-            }
-          n--;  
-        }
-        
- return false;
+        int n = s.size();
 
+        while(n--) {
+            rotate(s.begin(), s.begin() + 1, s.end());
+
+            if(s == goal)
+                return true;
+        }
+
+        return false;
     }
-   
 };
