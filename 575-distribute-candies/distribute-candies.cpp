b20dfc960ad64d33b404cebf117909721353cc8c
vkr-007 <vikasrajput120406@gmail.com>
2026-08-26 13:01:18 +0530

    Time: 114 ms (41.05%) | Memory: 120 MB (59.74%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
         int n=candyType.size()/2;
-        set<int> types;
+        unordered_set<int> types;
         for(auto x: candyType){
            types.insert(x);
         }
-        return (types.size()>=n) ?n:types.size();
+        return min((int)types.size(),n);
     }
 };
