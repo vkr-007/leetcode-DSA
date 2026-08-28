df2b9c5cd3c8a9b936687a6932e8db419436c1cc
vkr-007 <vikasrajput120406@gmail.com>
2026-08-28 12:29:50 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (76.90%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string temp= s;
+
+        for(int i=0;i<s.size();i++){
+            s[indices[i]]=temp[i];
+        } 
+        return s;
+    }
+};
