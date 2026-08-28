5e6982be54b6e94dd0ae0ec72f86a0f449096cf6
vkr-007 <vikasrajput120406@gmail.com>
2026-08-28 12:00:33 +0530

    Time: 6 ms (37.02%) | Memory: 15.2 MB (12.76%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void permute(vector<int>& nums, int idx,vector<vector<int>> &ans){
+        if(idx==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        unordered_set<int> used;
+        for(int i= idx;i<nums.size();i++){
+            if(used.count(nums[i])){
+                continue;
+            }
+            used.insert(nums[i]);
+            swap(nums[idx],nums[i]);
+            permute(nums,idx+1,ans);
+            swap(nums[idx],nums[i]);
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+         vector<vector<int>> ans;
+        permute(nums,0,ans);
+        return ans;
+    }
+};
