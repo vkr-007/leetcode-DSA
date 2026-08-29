04d40d21adc9f930831dd84a1b9f2680942961b9
vkr-007 <vikasrajput120406@gmail.com>
2026-08-29 11:51:30 +0530

    Time: 58 ms (12.76%) | Memory: 8 MB (82.90%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+ void nextPermutation(string &s) {
+
+        int n = s.size();
+        int i = n - 2;
+
+        while (i >= 0 && s[i] >= s[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = n - 1;
+
+            while (s[j] <= s[i]) {
+                j--;
+            }
+            swap(s[i], s[j]);
+        }
+        reverse(s.begin() + i + 1,s.end());
+    }
     string getPermutation(int n, int k) {
     vector<string> v = {
     "1",
