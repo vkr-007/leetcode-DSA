fefe9afc850f37de87b4e5599e3d109f1de05b96
vkr-007 <vikasrajput120406@gmail.com>
2026-08-29 12:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (58.17%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
- void nextPermutation(string &s) {
-
-        int n = s.size();
-        int i = n - 2;
-
-        while (i >= 0 && s[i] >= s[i + 1]) {
-            i--;
-        }
-
-        if (i >= 0) {
-            int j = n - 1;
-
-            while (s[j] <= s[i]) {
-                j--;
-            }
-            swap(s[i], s[j]);
-        }
-        reverse(s.begin() + i + 1,s.end());
-    }
     string getPermutation(int n, int k) {
-    vector<string> v = {
-    "1",
-    "12",
-    "123",
-    "1234",
-    "12345",
-    "123456",
-    "1234567",
-    "12345678",
-    "123456789"
-};
-string s= v[n-1];
-while(k-1>0){
-    next_permutation(s.begin(),s.end());
-    k--;
-}
-return s;
+      vector<int> v;
+      for(int i=1;i<n+1;i++){
+        v.push_back(i);
+      }
+      int fact=1;
+      for(int i=1;i<n;i++){
+        fact*=i;
+      }
+      k--;
+      string ans="";
+      while(!v.empty()){
+        int idx= k/fact;
+        ans+=to_string(v[idx]);
+        v.erase(v.begin()+idx);
+        k=k%fact;
+        if(v.size()>0){
+            fact/=v.size();
+        }
 
+      }
+      return ans;
     }
 };
