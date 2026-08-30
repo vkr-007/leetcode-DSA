12992ee636564e99d6e3de3b1cc0a3ffc81b0adb
vkr-007 <vikasrajput120406@gmail.com>
2026-08-30 10:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 92.1 MB (58.82%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -1,56 +1,23 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int b = *max_element(nums.begin(), nums.end());
-        int s = *min_element(nums.begin(), nums.end());
-        bool foundb = false;
-        bool founds = false;
-        int start = 0;
-        for (int x : nums) {
-            start++;
-            if (x == b)
-                foundb = true;
-            if (x == s)
-                founds = true;
+        int n = nums.size();
+        int mn = 0;
+        int mx = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < nums[mn]) {
+                mn = i;
+            }
+            if (nums[i] > nums[mx]) {
+                mx = i;
+            }
+        }
+        int front = min(mx, mn);
+        int back = max(mx, mn);
 
-            if (foundb && founds) {
-                foundb = false;
-                founds = false;
-
-                break;
-            }
-        }
-        int back = 0;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] == b)
-                foundb = true;
-            if (nums[i] == s)
-                founds = true;
-            back++;
-            if (foundb && founds) {
-                foundb = false;
-                founds = false;
-
-                break;
-            }
-        }
-        start = min(start, back);
-        int temp = 0;
-        for (auto x : nums) {
-            temp++;
-            if (x == b || x == s) {
-                break;
-            }
-        }
-        int j = nums.size() - 1;
-        while (j >= 0) {
-            temp++;
-            if (nums[j] == b || nums[j] == s) {
-                break;
-            }
-            j--;
-        }
-        return min(start, temp);
-        //te
+        int left = back + 1;
+        int right = n - front;
+        int both = (front + 1) + (n - back);
+        return min(left, min(right, both));
     }
 };
