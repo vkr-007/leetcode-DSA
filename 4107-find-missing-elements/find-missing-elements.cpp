7473914c647058178af61b38462f3e05b46b1322
vkr-007 <vikasrajput120406@gmail.com>
2026-09-19 11:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 33.8 MB (20.57%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int s= nums[0];
+        int l= nums[0];
+        set<int> n;
+        for(auto x:nums){ 
+            n.insert(x);
+            s=min(s,x);
+            l=max(l,x);
+        }
+        vector<int> ans;
+        for(int i=s;i<=l;i++){
+            if(n.find(i)==n.end()){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
