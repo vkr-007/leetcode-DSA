c5a808c6bb0d1aff89c6bc3ae8b151c351648314
vkr-007 <vikasrajput120406@gmail.com>
2026-09-21 23:54:14 +0530

    Time: 171 ms (32.14%) | Memory: 151.2 MB (36.61%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long long> result(k, 0);
+        vector<long long> prevcount(k, 0);
+        for (int i = 0; i < n; i++) {
+            vector<long long> currcount(k, 0);
+            int curr_er = nums[i] % k;
+            currcount[curr_er]++;
+            for (int old_r = 0; old_r < k; old_r++) {
+                int new_r = ((long long)old_r * nums[i] % k) % k;
+                currcount[new_r] += prevcount[old_r];
+            }
+            prevcount =currcount;
+            for (int x = 0; x < k; x++) {
+                result[x] += prevcount[x];
+            }
+        }
+        return result;
+    }
+};
