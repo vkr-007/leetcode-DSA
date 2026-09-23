9fc2b6f9189c44bb02f4a60059fb209641d171bd
vkr-007 <vikasrajput120406@gmail.com>
2026-09-23 21:55:29 +0530

    Time: 6 ms (12.10%) | Memory: 38.5 MB (14.91%) - LeetSync

--- 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int earliestTime(vector<vector<int>>& tasks) {
-        int ans=INT_MAX;
-        for(auto x: tasks){
-          ans=min(ans,x[0]+x[1]);
-        }
+        int ans = INT_MAX;
+        for (auto x : tasks) {ans = min(ans, x[0] + x[1]);}
         return ans;
     }
-    //temp
+    // still cant tell why code is not fast enough
 };
