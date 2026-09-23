e5eb578303b5beed8309b97f3b397e0b68d13513
vkr-007 <vikasrajput120406@gmail.com>
2026-09-23 21:49:21 +0530

    Time: 4 ms (17.58%) | Memory: 38.4 MB (19.41%) - LeetSync

---
+++ 4012-earliest-time-to-finish-one-task/earliest-time-to-finish-one-task.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int earliestTime(vector<vector<int>>& tasks) {
+        int ans=INT_MAX;
+        for(auto x: tasks){
+          ans=min(ans,x[0]+x[1]);
+        }
+        return ans;
+    }
+};
