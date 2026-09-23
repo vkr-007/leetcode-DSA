5967c506f842c9197f6db8fa7ff772faebeb73c5
vkr-007 <vikasrajput120406@gmail.com>
2026-09-23 10:45:46 +0530

    Time: 4 ms (60.60%) | Memory: 102.3 MB (65.24%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int t=0;
-       for(auto x:nums){
-          t+=x;
-       }
-       int j=0;
-       int temp=0;
-       int temp2=INT_MIN;
-       if(t<x) return -1;
-       if(t==x)return nums.size();
-       for(int i=0;i<nums.size();i++){
-           temp+=nums[i];
-           while(temp>t-x){
-            temp-=nums[j];
-            j++;
-           }
-          if(temp==t-x){
-             temp2=max(temp2,i-j+1);
-          }
-
-       }
-        return (temp2==INT_MIN)?-1:nums.size()-temp2;
+        int t = 0;
+        int n = nums.size();
+        for (auto x : nums) {
+            t += x;
+        }
+        if(t<x)return -1;
+        if(t==x) return n;
+        int j = 0;
+        int temp = 0;
+        int temp2 = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            temp += nums[i];
+            while (temp > t - x) {
+                temp -= nums[j];
+                j++;
+            }
+            if (temp == t - x) {
+                temp2 = max(temp2, i - j + 1);
+            }
+        }
+        return (temp2 == INT_MIN) ? -1 : n - temp2;
     }
 };
